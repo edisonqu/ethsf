296c98b669c1fbd7bafdabad100890c431ebae48
edison <edisonquu@gmail.com>
2022-11-05 01:59:24 -0700

    chek

---
+++ main.js
@@ -0,0 +1,20 @@
+
+
+/**
+ * 
+Json { 
+ChainName: ethereal, polygon
+condition: liquidity, price, method id
+level: liquidity amt, price, id
+Location: message, telegram
+username: XMTP username
+}
+ */
+function onSubmit(){
+    chainName = document.getElementsByClassName("chain");
+    condition = document.getElementsByClassName("condition")
+    level = document.getElementsByClassName("condition")
+    condition = document.getElementsByClassName("condition")
+    condition = document.getElementsByClassName("condition")
+
+}
